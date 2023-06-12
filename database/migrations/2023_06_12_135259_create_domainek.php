a371e414dddc7e4c4a05c7b1278bc8ed8a842a09
Molnár Tamás <molnarj@EOTVOS2.LOCAL>
2023-06-12 15:59:50 +0200

    létrehotam a domain táblát

--- database/migrations/2023_06_12_135259_create_domainek.php
+++ database/migrations/2023_06_12_135259_create_domainek.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('domainek', function (Blueprint $table) {
+            $table->id("d_id");
+            $table->string("domain_nev",30);
+            $table->string("domain_nev_human",30);
+            $table->date("lejarati_ido");
+            $table->dateTime("rogzitesi_ido");
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('domainek');
     }
 };
